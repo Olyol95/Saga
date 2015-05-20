af3f7c25da4d7ece71f216cd363258e6de7b6f28
Olyol95 <youle.oliver@gmail.com>
2015-05-20 01:41:27 +0100

    refactored packages, updated for 1.8.3

---
+++ src/org/saga/saveload/SagaCustomSerializer.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.saga.saveload;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+/**
+ * 
+ * @author Cory
+ */
+public class SagaCustomSerializer implements
+		JsonSerializer<SagaCustomSerialization>,
+		JsonDeserializer<SagaCustomSerialization> {
+
+	public SagaCustomSerialization deserialize(JsonElement je, Type type,
+			JsonDeserializationContext jdc) throws JsonParseException {
+
+		// Check if an object:
+		if (!je.isJsonObject()) {
+			throw new JsonParseException("JsonElement is not JsonObject");
+		}
+		JsonObject jo = (JsonObject) je;
+
+		// Custom class:
+		JsonElement classElement = jo.get("_className");
+		if (classElement == null)
+			throw new JsonParseException("missing _className field");
+		String className = classElement.getAsString();
+
+		// Find custom class:
+		try {
+			type = Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			throw new JsonParseException("class " + className + " not found");
+		}
+
+		// Get object:
+		SagaCustomSerialization object = jdc.deserialize(je, type);
+
+		// // Run complete:
+		// try {
+		// Method complete = object.getClass().getMethod("complete");
+		// complete.invoke(object);
+		// }
+		// catch (Exception e) {}
+
+		return object;
+
+	}
+
+	public JsonElement serialize(SagaCustomSerialization t, Type type,
+			JsonSerializationContext jsc) {
+
+		JsonObject jo = (JsonObject) jsc.serialize(t, t.getClass());
+
+		jo.addProperty("_className", t.getClass().getName());
+
+		return jo;
+
+	}
+
+}
---
+++ src/org/saga/shape/ShapeFilter.java
@@ -0,0 +1,16 @@
+package org.saga.shape;
+
+import org.bukkit.block.Block;
+
+public interface ShapeFilter {
+
+	/**
+	 * Check if the block is part of the shape.
+	 * 
+	 * @param block
+	 *            block
+	 * @return true if should be included in the shape
+	 */
+	boolean checkBlock(Block block);
+
+}
